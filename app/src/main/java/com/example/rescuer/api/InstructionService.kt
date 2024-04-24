8e8a3726fde2b1730330f2dd1390d0220a8c24dc
HP <wailmessaoudi806@gmail.com>
2024-04-24 22:42:46 +0100

    I created the InjuryType enum class

--- app/src/main/java/com/example/rescuer/api/InstructionService.kt
+++ app/src/main/java/com/example/rescuer/api/InstructionService.kt
@@ -3,10 +3,13 @@ package com.example.rescuer.api
 import com.example.rescuer.data.model.Instructions
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface InstructionService {
     @GET("first-aid")
     suspend fun getInstructions(): Response<List<Instructions>>
-    @GET("first-aid/{symptoms}")
-    suspend fun getInstruction(): Response<Instructions>
+    @GET("first-aid")
+    suspend fun getInstruction(
+        @Query("symptoms") symptoms: List<String>
+    ): Response<Instructions>
 }
---
+++ app/src/main/java/com/example/rescuer/data/model/enum_classes/InjuryType.kt
@@ -0,0 +1,7 @@
+package com.example.rescuer.data.model.enum_classes
+
+enum class InjuryType(val remoteName: String) {
+    GUNSHOT_WOUND("GunshotWound(x)"),
+    BLAST_INJURY("BlastInjury(x)"),
+    BURN_INJURY("BurnInjury(x)"),
+}
