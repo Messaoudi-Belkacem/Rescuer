44c43673cf9a9cb66890c735142887280a0a4ca6
HP <wailmessaoudi806@gmail.com>
2024-04-24 22:20:13 +0100

    I created InjuryLocation enum class

---
+++ app/src/main/java/com/example/rescuer/data/model/enum_classes/InjuryLocation.kt
@@ -0,0 +1,8 @@
+package com.example.rescuer.data.model.enum_classes
+
+enum class InjuryLocation(val remoteName: String) {
+    HEAD("InjuryLocationHead(x)"),
+    CHEST("InjuryLocationChest(x)"),
+    ABDOMEN("InjuryLocationAbdomen(x)"),
+    LIMBS("InjuryLocationLimbs(x)")
+}
