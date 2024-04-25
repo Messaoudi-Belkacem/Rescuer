82b19abf7442998d26f6c2389b71baa8be2d0ea3
HP <wailmessaoudi806@gmail.com>
2024-04-25 10:44:17 +0100

    I improved imports

--- app/src/main/java/com/example/rescuer/screen/fai/ResultScreen.kt
+++ app/src/main/java/com/example/rescuer/screen/fai/ResultScreen.kt
@@ -1,15 +1,10 @@
 package com.example.rescuer.screen.fai
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
